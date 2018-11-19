84ae9daa6e0158b3e775080e39ead2010d565386
Anton Myrholm <anton.myrholm@hav.havochvatten.se>
2018-11-19 11:15:51 +0100

    Added image controller

---
+++ arcgiscon_image_controller.py
@@ -0,0 +1,17 @@
+from PyQt4 import QtGui
+from PyQt4.QtCore import QObject
+from qgis.core import QgsMessageLog
+from arcgiscon_service import FileSystemService
+import os.path
+
+class ImageController(QObject):
+    _iface = None
+
+    def __init__(self, iface):
+        self._iface = iface
+
+    def saveImage(self, srcPath):
+        fileExt = os.path.splitext(srcPath)[1]
+        dstPath = QtGui.QFileDialog.getSaveFileName(caption = 'Save layer as image', filter = '*' + fileExt)
+        if len(dstPath) > 0:
+            FileSystemService().saveImageAs(srcPath, dstPath)
