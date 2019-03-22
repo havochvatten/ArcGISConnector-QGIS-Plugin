38fdd2b6d9fad44343a6723da6ef1be8aa5ede69
Anton Myrholm <anton.myrholm@hav.havochvatten.se>
2019-03-22 14:30:20 +0100

    Removed large head comments, thanks to all who contributed

--- __init__.py
+++ __init__.py
@@ -1,48 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-/***************************************************************************
- ArcGisImageServerConnector
-                                 A QGIS plugin
- ArcGIS REST API ImageServer Connector
-                             -------------------
-        begin                : 2018-08-31
-        copyright            : (C) 2018 by Anton Myrholm, Swedish Agency for Marine and Water Management, SwAM.
-        email                : anton.myrholm@havochvatten.se
-        git sha              : $Format:%H$
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
-Original by:
-
-/***************************************************************************
- ArcGisConnector
-                                 A QGIS plugin
- ArcGIS REST API Connector
-                             -------------------
-        begin                : 2015-05-27
-        copyright            : (C) 2015 by geometalab
-        email                : geometalab@gmail.com
-        git sha              : $Format:%H$
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
- This script initializes the plugin, making it known to QGIS.
-"""
 from __future__ import absolute_import
 
 # noinspection PyPep8Naming
--- arcgiscon_plugin_dialog.py
+++ arcgiscon_plugin_dialog.py
@@ -1,26 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-/***************************************************************************
- ArcGisConnectorDialog
-                                 A QGIS plugin
- ArcGIS REST API Connector
-                             -------------------
-        begin                : 2015-05-27
-        git sha              : $Format:%H$
-        copyright            : (C) 2015 by geometalab
-        email                : geometalab@gmail.com
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-"""
-
 import os
 
 from PyQt5 import uic
@@ -32,7 +10,6 @@ FORM_CLASS, _ = uic.loadUiType(os.path.join(
 
 class ArcGisConnectorDialog(QDialog, FORM_CLASS):
     def __init__(self, parent=None):
-        """Constructor."""
         super(ArcGisConnectorDialog, self).__init__(parent)
         # Set up the user interface from Designer.
         # After setupUI you can access any designer object by doing
